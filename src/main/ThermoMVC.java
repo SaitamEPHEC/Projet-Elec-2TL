442d361c754967ee670676322c3f078993b8c41f
SaitamEPHEC <m.gassmann@students.ephec.be>
2019-05-04 18:23:22 +0200

    ajout paramètres au modèle dans le main

--- src/main/ThermoMVC.java
+++ src/main/ThermoMVC.java
@@ -9,7 +9,7 @@ public class ThermoMVC {
 	public ThermoMVC() {
 			
 			//Création du modèle
-			Temperature modele = new Temperature();
+			Temperature modele = new Temperature(42,69);
 			
 			//Création du controleur
 			//Le contrôleur doit avoir une référence vers le modèle pour pouvoir le commander
